c9a2f040b4a110e635435adcfbeecb863bf87e63
rdminfosolution <rdminfosolution@gmail.com>
2020-10-12 12:05:00 +0530

    Updates

---
+++ precache-manifest.daf7b9ffa1341be8b621f881f98ad30c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3af4794bec9daa49860f74df57c8669",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4bd92e40fd102ff3767",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c5d2c5edc43de11b8de4",
+    "url": "/static/js/2.3ce95752.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3ce95752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4bd92e40fd102ff3767",
+    "url": "/static/js/main.7fa54c19.chunk.js"
+  },
+  {
+    "revision": "88dbc35c70fd6e8a6901",
+    "url": "/static/js/runtime-main.a911f084.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
